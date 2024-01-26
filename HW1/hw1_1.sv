305881d5e81e06ce0e7eeeb19eae05b351a8737c
unknown <carson.sager@okstate.edu>
2024-01-26 16:05:43 -0600

    update hw1

--- HW1/hw1_1.sv
+++ HW1/hw1_1.sv
@@ -1,6 +1,6 @@
 module sample (input logic [63:0] a, b, c, input logic signed [63:0] as, bs, cs, output logic [127:0] z, output logic signed [127:0] zs);
 
-    assign z = a*b+c;
-    assign zs = as*bs+cs
+    assign z = a*b+{64'h0, c};
+    assign zs = as*bs+{64'h0, cs};
 
 endmodule // sample
