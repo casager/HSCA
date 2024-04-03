4af895b2e8d16e44aa9af694440a844cbadd68d6
unknown <carson.sager@okstate.edu>
2024-04-03 16:20:49 -0500

    Project

--- Project/fpdiv.sv
+++ Project/fpdiv.sv
@@ -4,17 +4,18 @@ module fpdiv(num, denom, clk, reset, en_a, en_b, en_c, out);
 
     output logic [22:0] out; 
 
-    logic [25:0] ia_out, rega_out, regb_out, regc_out, mux2_out, mux4_out, oc_out
-    logic [51:0] mul_out
+    logic [26:0] ia_out, rega_out, regb_out, regc_out, mux2_out, mux4_out, oc_out
+    logic [53:0] mul_out
     logic sel_mux2, sel_mux4, sticky, ulp;
 
     //assign ia_out = 24'h60_0000; //can change to "better" guess
-    assign ia_out = 24'hc0_0000; //should represent 0.75
-    mux2 #(26) mux2(ia_out, regc_out, sel_mux2, mux2_out);
-    mux4 #(26) mux4(num, denom, rega_out, regb_out, sel_mux4, mux4_out);
+    assign ia_out = 27'h60_0000; //should represent 0.75
+    mux2 #(27) mux2(ia_out, regc_out, sel_mux2, mux2_out);
+    mux4 #(27) mux4(num, denom, rega_out, regb_out, sel_mux4, mux4_out);
     //multiply module
     assign mul_out = mux2_out * mux4_out;
     
+    //do not need either of these
     //determine ulp for rne (G * (L + R + sticky))
     assign sticky = |mul_out[20:0];
     // assumption is 23 is int bit, 21 is round bit (0.11), and sticky is past that
@@ -30,10 +31,10 @@ module fpdiv(num, denom, clk, reset, en_a, en_b, en_c, out);
     assign oc_out = ~mul_out;
 
     //regs (change TC to OC here as well)
-    flopenr #(26) rega(clk, reset, en_a, mul_out[51:26], rega_out);//chop bottom half mul_out
-    flopenr #(26) regb(clk, reset, en_b, mul_out[51:26], regb_out);
-    flopenr #(26) regc(clk, reset, en_c, oc_out[51:26], regc_out);
+    flopenr #(27) rega(clk, reset, en_a, mul_out[53:27], rega_out);//chop bottom half mul_out
+    flopenr #(27) regb(clk, reset, en_b, mul_out[53:27], regb_out);
+    flopenr #(27) regc(clk, reset, en_c, oc_out[53:27], regc_out);
     //assign q = rega_out;
-    assign out = mul_out[51:29];
+    //assign out = mul_out[51:29]; //will need an output of 23 bits but not until end
 
 endmodule //fpdiv
